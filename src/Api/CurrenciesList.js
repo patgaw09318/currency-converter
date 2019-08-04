16284d84ebbf16c0c128d6f6b9a36548e6d58311
patgaw09318 <patgaw09318@gmail.com>
2019-08-04 19:00:10 +0200

    axios-cache-adapter added

--- src/Api/CurrenciesList.js
+++ src/Api/CurrenciesList.js
@@ -1,12 +1,22 @@
 import axios from "axios";
+import { setupCache } from "axios-cache-adapter";
 import Config from "./Config";
 const Currencies = Config.Currencies;
 
-const CurrenciesList = () => {
-  axios.get(Currencies.url + Currencies.methods.currencies).then(res => {
-    console.log(res.data);
-    return res.data;
-  });
-};
+// Create `axios-cache-adapter` instance
+const cache = setupCache({
+  maxAge: 15 * 60 * 1000
+});
+
+const api = axios.create({
+  adapter: cache.adapter
+});
+
+const CurrenciesList = api({
+  url: Currencies.url + Currencies.methods.currencies,
+  method: "get"
+}).then(async response => {
+  return await response.data;
+});
 
 export default CurrenciesList;

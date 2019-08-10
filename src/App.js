f0b3c6e1f079c0fc41fb4a8301f889fde5e6edac
Patryk Gawryszewski <patgaw09318@gmail.com>
2019-08-10 21:05:32 +0200

    input - small improvements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import CurrenciesPanel from "./components/CurrenciesPanel";
 
+const onChange = (id)=>{
+  console.log(id);
+};
+
 function App() {
   return <div className="App" >
     <div className="Title">Currency converter</div>
-    <CurrenciesPanel id="from"></CurrenciesPanel>
-    <CurrenciesPanel id="to"></CurrenciesPanel>
+    <CurrenciesPanel id="from" onChange={onChange}></CurrenciesPanel>
+    <CurrenciesPanel id="to" onChange={onChange}></CurrenciesPanel>
   </div>;
 }
 
--- src/components/CurrenciesPanel.js
+++ src/components/CurrenciesPanel.js
@@ -4,13 +4,14 @@ import PropTypes from 'prop-types';
 
 const CurrenciesPanel = (props)=>(
     <div id={`${props.id}_panel`}>
-        <input></input>
+        <input id={`${props.id}_input`} type="number" step="any" placeholder="0.00" onChange={()=>props.onChange(`${props.id}_input`)}></input>
         <CurrenciesList id={`${props.id}`}></CurrenciesList>
     </div>
 )
 
 CurrenciesPanel.propTypes = {
-    id: PropTypes.string.isRequired
+    id: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
 }
 
 export default CurrenciesPanel;

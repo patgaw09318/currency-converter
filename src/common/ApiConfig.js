fc88475b7e29cac3761de6f0efbd31c05e4a9e1c
Patryk Gawryszewski <patgaw09318@gmail.com>
2019-08-15 19:55:48 +0200

    Files - formated by Prettier

--- src/common/ApiConfig.js
+++ src/common/ApiConfig.js
@@ -1,11 +1,10 @@
 const ApiConfig = {
-    ExchangeRates: {
-      url: "https://api.exchangeratesapi.io/"
-    },
-    Currencies: {
-      url: "https://openexchangerates.org/api/currencies.json"      
-    }
-  };
-  
-  export default ApiConfig;
-  
+  ExchangeRates: {
+    url: "https://api.exchangeratesapi.io/"
+  },
+  Currencies: {
+    url: "https://openexchangerates.org/api/currencies.json"
+  }
+};
+
+export default ApiConfig;
--- src/components/RatePanel.js
+++ src/components/RatePanel.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { useStateValue } from '../StateManager/StateContext';
+import React from "react";
+import { useStateValue } from "../StateManager/StateContext";
 
-const RatePanel = ()=> {
-    const [{ rate }] = useStateValue();
-    return  <div className="RatePanel">
-                <div>Rate:</div>
-                <div>{rate.value}</div>
-                <div>Date:</div>
-                <div>{rate.date}</div>
-            </div>
-}
+const RatePanel = () => {
+  const [{ rate }] = useStateValue();
+  return (
+    <div className="RatePanel">
+      <div>Rate:</div>
+      <div>{rate.value}</div>
+      <div>Date:</div>
+      <div>{rate.date}</div>
+    </div>
+  );
+};
 
 export default RatePanel;
